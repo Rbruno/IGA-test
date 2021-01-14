4ea89b592abde9d9e7b6b1547f8305691a8560e0
Rbruno <rodrigobruno74@gmail.com>
2021-01-14 12:27:08 +0100

    Error de subida

--- Sin Deploy/src/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import GithubReducer from './GithubReducer';
-
-const rootReducer = combineReducers({
-    GithubData: GithubReducer
-});
-
-export default rootReducer;
